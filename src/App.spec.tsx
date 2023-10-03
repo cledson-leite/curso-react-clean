a94a2a15bb9b3dd82eb785249212794b4e3a1ad6
Cledson Leite <csbetsonline@gmail.com>
2023-10-03 15:23:41 -0300

    initial commit

---
+++ src/App.spec.tsx
@@ -0,0 +1,15 @@
+// describe('Jest', () => {
+//   it('testing jest', () => {
+//     expect(1).toBe(1);
+//   });
+// });
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('<App />', () => {
+  it('should display elements', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { name: /vite \+ react/i })).toBeInTheDocument();
+  });
+});
