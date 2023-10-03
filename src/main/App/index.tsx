32ae34415f32018f3ea83d54e6350f2936296f35
Cledson Leite <csbetsonline@gmail.com>
2023-10-03 15:35:19 -0300

    configure initial

---
+++ src/main/App/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as Styled from './styles'
+
+export const App: React.FC = () => {
+  return (
+    <Styled.Container>
+      <h1>App</h1>
+    </Styled.Container>
+  )
+}
